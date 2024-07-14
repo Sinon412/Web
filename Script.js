f0b347264eb1fd60577d0e9fe221a1682de2cebc
GDSpain <155786276+GDSpain@users.noreply.github.com>
2024-07-14 15:19:30 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const contenedor = document.getElementById('lista-jugadores');
+
+    function cargarContenido(url, elemento) {
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error en la carga del contenido: ' + response.statusText);
+                }
+                return response.text();
+            })
+            .then(data => {
+                elemento.innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Hubo un problema con la solicitud Fetch:', error);
+                elemento.innerHTML = "<p>Error al cargar el contenido.</p>";
+            });
+    }
+
+    if (contenedor) {
+        cargarContenido('contenido.html', contenedor);
+    } else {
+        console.error('No se encontró el contenedor con el ID "lista-jugadores".');
+    }
+});
